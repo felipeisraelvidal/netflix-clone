4b363f99bd4fa992d4973ef9c1f82515f380c6c9
Felipe Vidal <felipeisra.vidal@gmail.com>
2021-12-27 14:50:48 -0300

    Configure the navigation bar and add the home section header

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    let sectionTitles: [String] = [
+        "Trending Movies",
+        "Popular",
+        "Trending TVs",
+        "Upcoming Movies",
+        "Top Rated"
+    ]
+    
     private let homeFeedTable: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        tableView.register(HomeSectionHeader.self, forHeaderFooterViewReuseIdentifier: HomeSectionHeader.identifier)
         return tableView
     }()
 
@@ -24,8 +33,27 @@ class HomeViewController: UIViewController {
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
         
+        configureNavigationBar()
+        
         let headerView = HeroHeaderView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
+    }
+    
+    private func configureNavigationBar() {
+        var image = UIImage(named: "netflix_logo")
+        image = image?.withRenderingMode(.alwaysOriginal)
+        
+        let logoItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        logoItem.isEnabled = false
+        
+        navigationItem.leftBarButtonItem = logoItem
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        
+        navigationController?.navigationBar.tintColor = .white
     }
     
     override func viewDidLayoutSubviews() {
@@ -39,11 +67,19 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
+        return sectionTitles.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: HomeSectionHeader.identifier) as? HomeSectionHeader else { return nil }
+        
+        headerView.titleLabel.text = sectionTitles[section]
+        
+        return headerView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -58,4 +94,38 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return 200
     }
     
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
+    
 }
+
+#if DEBUG
+import SwiftUI
+struct HomeViewControllerPreviews: PreviewProvider {
+    static var previews: some View {
+        if #available(iOS 14.0, *) {
+            ContainerPreview()
+                .ignoresSafeArea()
+                .environment(\.colorScheme, .dark)
+        } else {
+            ContainerPreview()
+                .environment(\.colorScheme, .dark)
+        }
+    }
+    
+    struct ContainerPreview: UIViewControllerRepresentable {
+        typealias UIViewControllerType = UINavigationController
+        
+        func makeUIViewController(context: Context) -> UINavigationController {
+            let navController = UINavigationController(rootViewController: HomeViewController())
+            return navController
+        }
+        
+        func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {}
+    }
+}
+#endif
---
+++ Netflix Clone/Views/HomeSectionHeader.swift
@@ -0,0 +1,46 @@
+//
+//  HomeSectionHeader.swift
+//  Netflix Clone
+//
+//  Created by Felipe Vidal on 27/12/21.
+//
+
+import UIKit
+
+class HomeSectionHeader: UITableViewHeaderFooterView {
+    
+    static let identifier = "HomeSectionHeader"
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .preferredFont(for: .title3, weight: .semibold)
+        label.text = "Section Title"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(titleLabel)
+        
+        applyConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func applyConstraints() {
+        let titleLabelConstraints = [
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ]
+        
+        NSLayoutConstraint.activate(titleLabelConstraints)
+    }
+
+}
