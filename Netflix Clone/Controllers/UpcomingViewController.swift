6297fcb601e150f453d393f5b77c9ac2814ff483
Felipe Vidal <felipeisra.vidal@gmail.com>
2021-12-27 22:56:08 -0300

    Update the UpcomingViewController title

--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -24,7 +24,7 @@ class UpcomingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Upcoming"
+        title = "Upcoming Soon"
 
         view.backgroundColor = .black
         
